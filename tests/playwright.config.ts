334e44337ee41a6070ebea4d50f584fb99e1bb30
Roman Starkov <sourcecode@roman.st>
2024-04-27 15:12:39 +0100

    Tests: update packages

--- tests/playwright.config.ts
+++ tests/playwright.config.ts
@@ -12,6 +12,7 @@ export default defineConfig({
     use: {
         baseURL: "http://localhost:6521", // Base URL to use in actions like `await page.goto('/')`
         trace: "on-first-retry", // Collect trace when retrying the failed test
+        //screenshot: 'only-on-failure',
         viewport: { width: 1000, height: 1000 },
         actionTimeout: 5000, // by default something like "click" never times out, making us wait for the whole test 30 sec timeout before failing
         locale: "en-GB",
